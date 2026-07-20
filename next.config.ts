aa3577482df2e9ee0f058ffbb112f554371ee906
Ritik Kumar <ritik.kumar@syntellite.com>
2026-07-20 16:17:38 +0530

    change

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,20 @@
 import type { NextConfig } from "next";
 
+const backendUrl = process.env.NODE_ENV === 'production' 
+  ? 'https://project-sewtech-mart.onrender.com' 
+  : 'http://127.0.0.1:8000';
+
 const nextConfig: NextConfig = {
   transpilePackages: ["recharts"],
   async rewrites() {
     return [
-  
        {
         source: "/api/v1/:path*",
-        destination: "http://127.0.0.1:8000/api/v1/:path*",
+        destination: `${backendUrl}/api/v1/:path*`,
       },
       {
         source: "/api/:path*",
-        destination: "http://127.0.0.1:8000/api/v1/:path*",
+        destination: `${backendUrl}/api/v1/:path*`,
       },
     ];
   },
